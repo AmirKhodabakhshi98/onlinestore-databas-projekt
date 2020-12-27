0257fc56fe5cafcc26cdfc127ddd5318259b19a1
AmirKhodabakhshi1998 <62739801+AmirKhodabakhshi98@users.noreply.github.com>
2020-12-27 18:23:15 +0100

    setup

---
+++ src/control.java
@@ -0,0 +1,25 @@
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class control {
+
+    public control() {
+
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println("test");
+
+
+
+
+    }
+}
---
+++ src/userProcedures.java
@@ -0,0 +1,2 @@
+public class userProcedures {
+}
