5a2215850e373873bc7a287ef7c9831be6495826
AmirKhodabakhshi1998 <62739801+AmirKhodabakhshi98@users.noreply.github.com>
2021-01-21 16:57:02 +0100

    wallah funka

--- src/model/Product.java
+++ src/model/Product.java
@@ -9,14 +9,20 @@ public class Product {
     //Add a new product
     public void addProduct(int id, String supplier, String name, int price, int quantity ){
 
-        query =  " ";
+        query =  "Insert into Product (Product_id, Supplier_name, Name, Base_price, Quantity) " +
+                "values (" + id + ", " + supplier + ", " + name + ", " + price + ", " + quantity  + ");";
 
         Connection.executeQuery(query);
-
     }
 
+    public void editProductQuantity(int id, int quantity){
 
+        query = "Update product " +
+                "Set Quantity = " + quantity +
+                "Where Product_id = " + id;
+        Connection.executeQuery(query);
 
 
     }
+}
 
