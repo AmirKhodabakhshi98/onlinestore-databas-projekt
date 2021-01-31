a802038cbbc1d8cd72d9e95549498423421e0e44
yousef <yousefkhalil125@gmail.com>
2021-01-31 21:40:47 +0100

    asd

--- sql commands/tables/createProductTable.sql
+++ sql commands/tables/createProductTable.sql
@@ -1,7 +1,7 @@
 CREATE TABle Product (
     Product_id int NOT NULL AUTO_INCREMENT,
-    Supplier_name nvchar(50) NOT NULL,
-    Name nvchar(50) NOT NULL,
+    Supplier_name nvarchar(50) NOT NULL,
+    Name nvarchar(50) NOT NULL,
     Base_price int NOT NULL,
     QUANTITY int NOT NULL,
     PRIMARY KEY (Product_id)
