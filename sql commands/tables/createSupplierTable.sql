c6035311b647c148fe7bb31ce7557efb3b17c76a
yousef <yousefkhalil125@gmail.com>
2021-01-31 21:38:24 +0100

    as

--- sql commands/tables/createSupplierTable.sql
+++ sql commands/tables/createSupplierTable.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Supplier (
-    Name nvchar(30) NOT NULL,
+    Name nvarchar(30) NOT NULL,
     Phone_number int NOT NULL,
-    Address nvchar(50) NOT NULL,
+    Address nvarchar(50) NOT NULL,
     PRIMARY KEY (Name)
 )
