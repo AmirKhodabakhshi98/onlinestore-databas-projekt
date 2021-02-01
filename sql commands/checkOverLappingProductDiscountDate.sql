72e38e2840e9cab8a0be8e34ed069cb9378caec5
AmirKhodabakhshi1998 <62739801+AmirKhodabakhshi98@users.noreply.github.com>
2021-02-01 17:26:18 +0100

    Update checkOverLappingProductDiscountDate.sql

--- sql commands/checkOverLappingProductDiscountDate.sql
+++ sql commands/checkOverLappingProductDiscountDate.sql
@@ -12,12 +12,11 @@ begin
     	if(@startDate>@endDate) return 0;
 
     	if ((select count(*) from Product_discount
-    	where [Product_id] = @productId
-    	and @startDate <=  [End_date]
-    	and @endDate >= [Start_date]) > 1)
+    	     where [Product_id] = @productId
+        	    and @startDate <=  [End_date]
+    	        and @endDate >= [Start_date]) > 1)
            return 0;
 
-       
 
     return 1;
 
