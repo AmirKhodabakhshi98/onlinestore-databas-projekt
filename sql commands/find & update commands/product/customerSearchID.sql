afe41cafd61df4a5e9ef97dfef8843f1654376e1
AmirKhodabakhshi1998 <62739801+AmirKhodabakhshi98@users.noreply.github.com>
2021-03-25 23:56:15 +0100

    ghrebdgdgdfergdrthtrdhjtdjtrdyj

--- sql commands/find & update commands/product/customerSearchID.sql
+++ sql commands/find & update commands/product/customerSearchID.sql
@@ -4,14 +4,17 @@ CREATE PROCEDURE customerSearchID
 --Method for customer to search for a product based on its ID.
 AS
 
-select Product.Product_id, Product.Supplier_name, Product.Name, Product.Base_price, Product.Quantity, Discount.Description, Discount.Percentage
-from Product
-FULL OUTER JOIN Product_discount
-	on Product.Product_id = Product_discount.Product_id
-FULL OUTER JOIN Discount
-	on Discount.Discount_id = Product_discount.Discount_id
-WHERE ((Product_discount.Start_date <= CURRENT_TIMESTAMP AND Product_discount.End_date >= CURRENT_TIMESTAMP)
-		OR (Product.Product_id NOT IN (Select Product_discount.Product_id from Product_discount)))
-	AND Product.Product_id = @id
+SELECT
+Product.Product_id, Product.Supplier_name, Product.Name,
+Product.Base_price, Product.Quantity, x.Description, x.Percentage
+	FROM Product FULL OUTER JOIN(
+		SELECT Product.Product_id, Discount.Description, Discount.Percentage FROM Product
+		 JOIN Product_discount
+			ON Product.Product_id = Product_discount.Product_id
+		 JOIN DISCOUNT
+			ON Discount.Discount_id = Product_discount.Discount_id
+		WHERE (Product_discount.Start_date <= CURRENT_TIMESTAMP AND Product_discount.End_date >= CURRENT_TIMESTAMP)
+)x ON Product.Product_id = x.Product_id
+WHERE Product.Product_id = @id
 
 GO
--- sql commands/find & update commands/product/customerSearchSupplier.sql
+++ sql commands/find & update commands/product/customerSearchSupplier.sql
@@ -4,14 +4,20 @@ CREATE PROCEDURE customerSearchSupplier
 
 AS
 
-select Product.Product_id, Product.Supplier_name, Product.Name, Product.Base_price, Product.Quantity, Discount.Description, Discount.Percentage
-from Product
-FULL OUTER JOIN Product_discount
-	on Product.Product_id = Product_discount.Product_id
-FULL OUTER JOIN Discount
-	on Discount.Discount_id = Product_discount.Discount_id
-WHERE ((Product_discount.Start_date <= CURRENT_TIMESTAMP AND Product_discount.End_date >= CURRENT_TIMESTAMP)
-		OR (Product.Product_id NOT IN (Select Product_discount.Product_id from Product_discount)))
-	AND Product.Supplier_name = @supplier
+
+SELECT
+Product.Product_id, Product.Supplier_name, Product.Name,
+Product.Base_price, Product.Quantity, x.Description, x.Percentage
+	FROM Product FULL OUTER JOIN(
+		SELECT Product.Product_id, Discount.Description, Discount.Percentage FROM Product
+		 JOIN Product_discount
+			ON Product.Product_id = Product_discount.Product_id
+		 JOIN DISCOUNT
+			ON Discount.Discount_id = Product_discount.Discount_id
+		WHERE (Product_discount.Start_date <= CURRENT_TIMESTAMP AND Product_discount.End_date >= CURRENT_TIMESTAMP)
+)x ON Product.Product_id = x.Product_id
+WHERE Product.Supplier_name = @supplier
 
 GO
+
+
